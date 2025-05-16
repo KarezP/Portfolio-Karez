e44d261e7025622b29290d7ca88252be943f9bcf
KarezP <karezpeshawa75@gmail.com>
2025-05-16 18:20:07 +0200

    Update AppFooterCopyright.jsx

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -2,17 +2,11 @@ function AppFooterCopyright() {
 	return (
 		<div className="font-general-regular flex justify-center items-center text-center">
 			<div className="text-lg text-ternary-dark dark:text-ternary-light">
-				&copy; {new Date().getFullYear()}
-				<a
-					href="https://github.com/realstoman/react-tailwindcss-portfolio"
-					target="__blank"
-					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					© 2025 | Portfolio by Karez – Frontend Developer.
-				</a>
+				&copy; {new Date().getFullYear()} | Portfolio by Karez – Frontend Developer.
 			</div>
 		</div>
 	);
 }
 
 export default AppFooterCopyright;
+
